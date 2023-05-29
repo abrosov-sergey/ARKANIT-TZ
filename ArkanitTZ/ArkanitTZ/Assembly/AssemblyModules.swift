ba867a9271f1c509331fb96b02c9ea15a89690e7
Сергей Абросов <abrosovsergeya@gmail.com>
2023-05-29 22:28:50 +0300

    Added Swinject&SnapKit

---
+++ ArkanitTZ/ArkanitTZ/Assembly/AssemblyModules.swift
@@ -0,0 +1,20 @@
+//
+//  Assembly.swift
+//  ArkanitTZ
+//
+//  Created by Сергей Абросов on 29.05.2023.
+//
+
+import Foundation
+import Swinject
+
+@objc public class Assembly: NSObject {
+  private static let assemblies: [Swinject.Assembly] = [
+    
+  ]
+
+  private static let container = Container()
+  private static let assembler = Assembler(assemblies, container: container)
+
+  static let resolver = (assembler.resolver as! Container).synchronize()
+}
